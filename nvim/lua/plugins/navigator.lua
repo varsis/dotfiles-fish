abb0895955d0fd0c0677767e16d4750e0683755d
Chris Pavlicek <chris@Chriss-MacBook-Pro.local>
2025-09-13 20:28:27 +0100

    fix: open code and tmux

--- nvim/lua/plugins/navigator.lua
+++ nvim/lua/plugins/navigator.lua
@@ -9,10 +9,16 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
     { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
   },
+  config = function()
+    vim.keymap.set("t", "<C-h>", "<C-\\><C-n>:TmuxNavigateLeft<cr>")
+    vim.keymap.set("t", "<C-j>", "<C-\\><C-n>:TmuxNavigateDown<cr>")
+    vim.keymap.set("t", "<C-k>", "<C-\\><C-n>:TmuxNavigateUp<cr>")
+    vim.keymap.set("t", "<C-l>", "<C-\\><C-n>:TmuxNavigateRight<cr>")
+  end,
 }
