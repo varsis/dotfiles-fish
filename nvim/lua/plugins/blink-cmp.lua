315c5bd295b50ccd116ce086486c04f652705385
Chris Pavlicek <chris@varsisstudio.com>
2026-02-20 21:37:10 +0000

    feat(blink): update snippet selection

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -8,7 +8,21 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "default",
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.snippet_forward()
+          else
+            return cmp.accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    },
     appearance = {
       -- use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
