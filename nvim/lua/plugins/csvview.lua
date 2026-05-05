c9941f0672ad0fa3264117ce2cd66ad283a17e65
Chris Pavlicek <chris@varsisstudio.com>
2026-05-05 08:05:40 +0100

    feat(csv): add csv viewer

---
+++ nvim/lua/plugins/csvview.lua
@@ -0,0 +1,8 @@
+return {
+  "hat0uma/csvview.nvim",
+  ft = { "csv", "tsv" },
+  opts = {},
+  keys = {
+    { "<leader>ct", "<cmd>CsvViewToggle<cr>", desc = "Toggle CSV view", ft = { "csv", "tsv" } },
+  },
+}
